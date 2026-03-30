bb0b5aa308da0b14b1b5263a1a0534d265d169a4
Maciej Duda <maciejduda1@icloud.com>
2026-03-30 11:20:39 +0200

    implementation of expression interface for AST

--- src/representation/expr.go
+++ src/representation/expr.go
@@ -1,3 +1,35 @@
 package representation
 
-import "github.com/kolaowalska/loxxy/src/scanner"
+import "github.com/kolaowalska/loxxy/src/scanning"
+
+// Expr - Base interface for all expression nodes.
+type Expr interface {
+	exprNode() // dummy method
+}
+
+type Binary struct {
+	Left     Expr
+	Operator scanner.Token
+	Right    Expr
+}
+
+func (b *Binary) exprNode() {}
+
+type Grouping struct {
+	Expression Expr
+}
+
+func (g *Grouping) exprNode() {}
+
+type Literal struct {
+	Value any
+}
+
+func (l *Literal) exprNode() {}
+
+type Unary struct {
+	Operator scanner.Token
+	Right    Expr
+}
+
+func (u *Unary) exprNode() {}

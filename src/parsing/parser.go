13fdfdb5390934da1b8d491225d032d638eb5cba
KOLA <normalnie.kowalska@gmail.com>
2026-04-06 15:48:28 +0200

    added method stubs for grammar rules

--- src/parsing/parser.go
+++ src/parsing/parser.go
@@ -1,30 +1,52 @@
 package parsing
 
-import scanner "github.com/kolaowalska/loxxy/src/scanning"
+import (
+	"github.com/kolaowalska/loxxy/src/representation"
+	scanner "github.com/kolaowalska/loxxy/src/scanning"
+)
 
 type Parser struct {
-	tokens   []scanner.Token
-	current  int
-	reporter scanner.ErrorReporter
+	tokens  []scanner.Token
+	current int
 }
 
-func NewParser(tokens []scanner.Token, reporter scanner.ErrorReporter) *Parser {
+func NewParser(tokens []scanner.Token) *Parser {
 	return &Parser{
-		tokens:   tokens,
-		reporter: reporter,
+		tokens:  tokens,
+		current: 0,
 	}
 }
 
+func (p *Parser) expression() (representation.Expr, error) {
+	return p.equality()
+}
+
 func (p *Parser) match(types ...scanner.TokenType) bool {
+	for _, t := range types {
+		if p.check(t) {
+			p.advance()
+			return true
+		}
+	}
 	return false
 }
 
 func (p *Parser) check(t scanner.TokenType) bool {
-	return false
+	if p.isAtEnd() {
+		return false
+	}
+	return p.peek().TokenType == t
 }
 
 func (p *Parser) advance() scanner.Token {
-	return p.tokens[p.current]
+	if !p.isAtEnd() {
+		p.current++
+	}
+	return p.previous()
+}
+
+func (p *Parser) isAtEnd() bool {
+	return p.peek().TokenType == scanner.EOF
 }
 
 func (p *Parser) peek() scanner.Token {
@@ -33,4 +55,29 @@ func (p *Parser) peek() scanner.Token {
 
 func (p *Parser) previous() scanner.Token {
 	return p.tokens[p.current-1]
+}
+
+// TODO
+func (p *Parser) equality() (representation.Expr, error) {
+	return nil, nil
+}
+
+func (p *Parser) comparison() (representation.Expr, error) {
+	return nil, nil
+}
+
+func (p *Parser) term() (representation.Expr, error) {
+	return nil, nil
+}
+
+func (p *Parser) factor() (representation.Expr, error) {
+	return nil, nil
+}
+
+func (p *Parser) unary() (representation.Expr, error) {
+	return nil, nil
+}
+
+func (p *Parser) primary() (representation.Expr, error) {
+	return nil, nil
 }

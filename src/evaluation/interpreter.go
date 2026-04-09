f5c99b6799539011ea31db3281d5fc027cdb5854
KOLA <normalnie.kowalska@gmail.com>
2026-04-09 17:59:34 +0200

    add truth check function for the interpreter

--- src/evaluation/interpreter.go
+++ src/evaluation/interpreter.go
@@ -5,3 +5,25 @@ import "github.com/kolaowalska/loxxy/src/representation"
 func Evaluation(expr representation.Expr) (any, error) {
 	return nil, nil
 }
+
+func isTruthy(obj any) bool {
+	if obj == nil {
+		return false
+	}
+	if b, ok := obj.(bool); ok {
+		return b
+	}
+	return true
+}
+
+// unnecessary due to go's two-step automatic check
+//
+//func isEqual(a any, b any) bool {
+//	if a == nil && b == nil {
+//		return true
+//	}
+//	if a == nil {
+//		return false
+//	}
+//	return a == b
+//}

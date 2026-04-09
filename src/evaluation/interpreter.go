a788dbdd6b9c31363e3042a9f445318ce33d27dd
Maciej Duda <maciejduda1@icloud.com>
2026-04-09 18:13:15 +0200

    evaluate function implementation for future usage in run function in main

--- src/evaluation/interpreter.go
+++ src/evaluation/interpreter.go
@@ -1,7 +1,94 @@
 package evaluation
 
-import "github.com/kolaowalska/loxxy/src/representation"
+import (
+	"fmt"
 
-func Evaluation(expr representation.Expr) (any, error) {
-	return nil, nil
+	"github.com/kolaowalska/loxxy/src/representation"
+	scanner "github.com/kolaowalska/loxxy/src/scanning"
+)
+
+func Evaluate(expr representation.Expr) (any, error) {
+	switch e := expr.(type) {
+
+	case *representation.Literal:
+		return e.Value, nil
+
+	case *representation.Grouping:
+		return Evaluate(e.Expression)
+
+	case *representation.Unary:
+		right, err := Evaluate(e.Right)
+		if err != nil {
+			return nil, err
+		}
+
+		switch e.Operator.TokenType {
+
+		case scanner.MINUS:
+			return -right.(float64), nil
+
+		case scanner.BANG:
+			return !isTruthy(right), nil
+
+		}
+
+	case *representation.Binary:
+		left, err := Evaluate(e.Left)
+		if err != nil {
+			return nil, err
+		}
+
+		right, err := Evaluate(e.Right)
+		if err != nil {
+			return nil, err
+		}
+
+		switch e.Operator.TokenType {
+
+		case scanner.MINUS:
+			return left.(float64) - right.(float64), nil
+
+		case scanner.SLASH:
+			if right.(float64) == 0 { //TODO: test if needed
+				return nil, nil //TODO: insert runtime error on e.Operator, "Cannot divide by zero."
+			}
+			return left.(float64) / right.(float64), nil
+
+		case scanner.STAR:
+			return left.(float64) * right.(float64), nil
+
+		case scanner.PLUS:
+			if l, ok := left.(float64); ok {
+				if r, ok := right.(float64); ok {
+					return l + r, nil
+				}
+			}
+			if l, ok := left.(string); ok {
+				if r, ok := right.(string); ok {
+					return l + r, nil
+				}
+			}
+			return nil, nil //TODO: runtime error on e.Operator, "Operands must be two numbers or two strings."
+
+		case scanner.GREATER:
+			return left.(float64) > right.(float64), nil
+
+		case scanner.GREATER_EQUAL:
+			return left.(float64) >= right.(float64), nil
+
+		case scanner.LESS:
+			return left.(float64) < right.(float64), nil
+
+		case scanner.LESS_EQUAL:
+			return left.(float64) <= right.(float64), nil
+
+		case scanner.BANG_EQUAL:
+			return left.(float64) != right.(float64), nil
+
+		case scanner.EQUAL_EQUAL:
+			return left.(float64) == right.(float64), nil
+		}
+	}
+
+	return nil, fmt.Errorf("Unknown expression type.")
 }

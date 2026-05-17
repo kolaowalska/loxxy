d1df40c99fdc1f6fca204a8ddd3dd9b4f1347232
KOLA <normalnie.kowalska@gmail.com>
2026-05-17 20:33:41 +0200

    debugger core + display

--- src/debugging/debugger.go
+++ src/debugging/debugger.go
@@ -1,1 +1,66 @@
 package debugging
+
+import (
+	"bufio"
+	"io"
+	"sort"
+	"strings"
+
+	"github.com/kolaowalska/loxxy/src/evaluation"
+)
+
+type Debugger struct {
+	breakpoints map[int]bool
+	stepMode    bool
+	sourceLines []string
+
+	in  *bufio.Reader
+	out io.Writer
+}
+
+func NewDebugger(in io.Reader, out io.Writer) *Debugger {
+	return &Debugger{
+		breakpoints: make(map[int]bool),
+		in:          bufio.NewReader(in),
+		out:         out,
+	}
+}
+
+func (d *Debugger) LoadSource(src string) {
+	d.sourceLines = strings.Split(src, "\n")
+}
+
+func (d *Debugger) SetBreakpoint(line int) {
+	d.breakpoints[line] = true
+}
+
+func (d *Debugger) DeleteBreakpoint(line int) {
+	delete(d.breakpoints, line)
+}
+
+func (d *Debugger) ListBreakpoints() []int {
+	lines := make([]int, 0, len(d.breakpoints))
+	for line := range d.breakpoints {
+		lines = append(lines, line)
+	}
+	sort.Ints(lines)
+	return lines
+}
+
+func (d *Debugger) OnStatement(
+	line int,
+	frames []evaluation.CallFrame,
+	env *evaluation.Environment,
+) {
+	if !d.stepMode && !d.breakpoints[line] {
+		return
+	}
+
+	d.stepMode = false
+	d.renderPause(line, frames, env)
+	d.commandLoop()
+}
+
+func (d *Debugger) commandLoop() {
+	// TODO (emipoprostu B) )
+}
--- src/debugging/display.go
+++ src/debugging/display.go
@@ -1,1 +1,95 @@
 package debugging
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+
+	"github.com/kolaowalska/loxxy/src/evaluation"
+)
+
+func (d *Debugger) renderSourceContext(line int) {
+	start := line - 3
+	if start < 1 {
+		start = 1
+	}
+
+	end := line + 2
+	if end > len(d.sourceLines) {
+		end = len(d.sourceLines)
+	}
+
+	for i := start; i <= end; i++ {
+		src := ""
+		if i-1 < len(d.sourceLines) {
+			src = d.sourceLines[i-1]
+		}
+		marker := "  "
+		if i == line {
+			marker = "→ "
+		}
+		_, err := fmt.Fprintf(d.out, "%s%3d | %s\n", marker, i, src)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (d *Debugger) renderLocals(env *evaluation.Environment) {
+	snap := env.Snapshot()
+	keys := make([]string, 0, len(snap))
+	for k := range snap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		_, err := fmt.Fprintf(d.out, "  %-12s = %v\n", k, snap[k])
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (d *Debugger) renderCallStack(frames []evaluation.CallFrame) {
+	for i, f := range frames {
+		marker := "  "
+		if i == 0 {
+			marker = "→ "
+		}
+		_, err := fmt.Fprintf(d.out, "  %s%-16s line %d\n", marker, f.Name, f.Line)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func (d *Debugger) renderPause(
+	line int,
+	frames []evaluation.CallFrame,
+	env *evaluation.Environment,
+) {
+	sep := strings.Repeat("—", 50)
+	_, err := fmt.Fprintf(d.out, "\n—— PAUSED — line %d %s\n\n", line, sep)
+	if err != nil {
+		return
+	}
+	d.renderSourceContext(line)
+	_, err = fmt.Fprintf(d.out, "\n—— LOCALS %s\n", sep)
+	if err != nil {
+		return
+	}
+	d.renderLocals(env)
+	_, err = fmt.Fprintf(d.out, "\n—— CALL STACK %s\n", sep)
+	if err != nil {
+		return
+	}
+	d.renderCallStack(frames)
+	_, err = fmt.Fprintf(d.out, "\n%s\n", sep)
+	if err != nil {
+		return
+	}
+	_, err = fmt.Fprintf(d.out, "[enter] step  [c] continue  [b N] breakpoint  [d N] delete  [q] quit\n")
+	if err != nil {
+		return
+	}
+}
